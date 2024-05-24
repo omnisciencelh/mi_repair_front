0164180546e57b26bfef847f27f3d8014764d61f
笙箫语陌 <3453479469@qq.com>
2024-05-24 14:55:59 +0800

    文件上传组件

---
+++ src/components/myComponents/upload-file.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="modal" v-show="showUpload">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <h2 style="text-align: center;">上传文件</h2>
+      <div v-if="fileType==='image'"></div>
+      <div v-if="fileType === 'video'">上传视频</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showUpload: {
+      type: Boolean,
+      default: false
+    },
+    orderId: {
+      type: String,
+      default: ''
+    },
+    fileType: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      dialogImageUrl: '',
+      dialogVisible: false,
+      disabled: false
+    }
+  },
+  created () {
+  },
+  methods: {
+    // 关闭遮盖层
+    closeModal () {
+      this.$emit('closeModal')
+    },
+    handleRemove (file) {
+      console.log(file)
+    },
+    handlePictureCardPreview (file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    // 上传图片
+    upload () {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .modal {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); /* 半透明背景 */
+    z-index: 9998; /* 确保z-index比其他元素高 */
+  }
+
+  .modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #888;
+    width: 40%; /* 或指定一个固定的宽度 */
+  }
+
+  .close {
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .close:hover,
+  .close:focus {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  .scroll-container {
+    display: flex;
+    overflow-x: auto;
+    white-space: nowrap;
+  }
+  .img {
+    max-width: 100%; /* 图片最大宽度为列宽 */
+    height: auto; /* 图片高度自动 */
+  }
+</style>
